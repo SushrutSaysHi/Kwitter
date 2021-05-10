fc2932cb29173b9b9aefa9456642abb0da3b7186
SushrutSaysHi <78794844+SushrutSaysHi@users.noreply.github.com>
2021-05-10 18:17:46 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,15 @@
+//ADD FIREBASE
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAvPj0FIorjjEKPHIbuuQEqu2_1aIQQWUU",
+    authDomain: "sushrut-ypam.firebaseapp.com",
+    databaseURL: "https://sushrut-ypam-default-rtdb.firebaseio.com",
+    projectId: "sushrut-ypam",
+    storageBucket: "sushrut-ypam.appspot.com",
+    messagingSenderId: "238204360898",
+    appId: "1:238204360898:web:166d46978068489ed6835b",
+    measurementId: "G-90VZBLDD3Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
